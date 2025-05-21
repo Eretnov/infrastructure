e231fb94cd713afa5a74a5a2ab78ce75798af243
aeretnov <student@fhmqu9qeib0kn1af7mtn.auto.internal>
2025-05-21 18:00:35 +0000

    Add modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = ">= 0.87.0"
-    }
-  }
+module "yandex_cloud_instance" {
+  source  = "./modules/tf-yc-instance"
+  ssh_key = var.ssh_key
+  zone    = var.zone
+  image_id = var.image_id
+  subnet_id = module.yandex_cloud_network.subnet_ids[var.zone]
 }
 
-provider "yandex" {
-  cloud_id  = "b1g3jddf4nv5e9okle7p"
-  folder_id = "b1g4n3h4dcqdt2vtjlsv"
-  zone      = "ru-central1-a"
-}
-
-resource "yandex_compute_instance" "vm-1" {
-  name = "chapter5-lesson2-std-040-31"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd80qm01ah03dkqb14lc"
-    }
-  }
-
-  network_interface {
-    subnet_id = "e9behps33a0dus6o9lmq"
-    nat       = true
-  }
-
-  metadata = {
-    user-data = templatefile("~/infrastructure/terraform/cloud-init.yaml.tmpl", {
-      ssh_key = var.ssh_key
-    })
-  }
-}
-
-output "ip_address" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+module "yandex_cloud_network" {
+  source = "./modules/tf-yc-network" 
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,21 @@
 variable "ssh_key" {
-  description = "Публичный SSH ключ"
   type        = string
+}
+
+variable "cloud_id" {
+  type        = string
+}
+
+variable "folder_id" {
+  type        = string
+}
+
+variable "zone" {
+  type        = string
+  default     = "ru-central1-a"
+}
+
+variable "image_id" {
+  type        = string
+  default     = "fd80qm01ah03dkqb14lc"
 }
